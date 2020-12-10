8e63675f2be3490d3b5f4fc47d19076e0c9f4dbb
tabatalisboa <tabatalisboa@protonmail.com>
2020-12-10 20:31:34 -0300

    Drop Devise

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_many :cocktails
-  has_many :doses
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
---
+++ db/migrate/20201210225823_drop_devise_tables.rb
@@ -0,0 +1,5 @@
+class DropDeviseTables < ActiveRecord::Migration[6.0]
+  def change
+    drop_table :users
+  end
+end
